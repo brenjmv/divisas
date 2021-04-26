eb8dd11ba042ff243d5ce276d1d051536c4de8de
brenjmv <76862244+brenjmv@users.noreply.github.com>
2021-04-26 16:27:44 -0500

    Update divisa.c

--- divisa.c
+++ divisa.c
@@ -5,24 +5,24 @@
 int a = 0;
 int operacion, resultado; 
 printf("dame la cantidad en pesos mexicanos: ");
-scanf("%d", &a);
+scanf("%f", &a);
 printf("seleccione la divisa a la que quiere convertir\n");
 printf(" 1.- Libra \n 2.- Dolar \n 3.- Euro \n");
-scanf("%d", &operacion)
+scanf("%f", &operacion)
 
 switch (operacion)
 {
 case 1:
     resultado = a * 0.036;
-    printf("El resultado es: %d * 0.036 = %d", a, resultado);
+    printf("El resultado es: %f * 0.036 = %df", a, resultado);
     break;
 case 2:
     resultado = a * 0.050;
-    printf("El resultado es: %d * 0.050 = %d", a, resultado);
+    printf("El resultado es: %f * 0.050 = %f", a, resultado);
     break;
 case 3: 
     resultado = a * 0.042;
-    printf("El resultado es: %d * 0.042 = %d", a, resultado);
+    printf("El resultado es: %f * 0.042 = %f", a, resultado);
     break;
 
 default:
